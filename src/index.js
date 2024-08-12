382b70f3be9a4842e76160c42dc1cfd42bda7fc4
Ray Silva <silvaray355@gmail.com>
2024-08-12 11:52:35 -0300

    Revert "init"
    This reverts commit 5d2f7e57995d2b561d0d2d37e06949b7ba2a4a09.

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const app = express();
-const port = process.env.PORT;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => console.log('Listen on http://localhost:' + port));
